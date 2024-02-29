3eb1d374683994c9ef386f82dfac72fa848e27bc
SANTYXGT <davidsantiagoguerrerotolosa@gmail.com>
2024-02-29 08:59:03 -0500

    Calculadora JAVA raiz cuadrada - Juan Pablo Velasquez Hernandez

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -35,7 +35,9 @@ public class Calculadora {
                 respuesta= numero1 % numero2;
                 break;
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-                
+            case 'r':
+                respuesta= Math.pow(numero1,(double)1/numero2);
+                break;
             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
@@ -53,7 +55,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /,^,%)");
+        System.out.println("Ingrese la operación (+, -, *, /,^,%,r)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
